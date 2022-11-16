af9055e78b2fd0face18006df4397e5a9263da48
PkrUlciferN <ta.pornpawit_st@tni.ac.th>
2022-11-16 13:46:07 +0700

    Basic Component

--- App.js
+++ App.js
@@ -3,13 +3,17 @@ import React from 'react'
 import Information from './components/Information'
 import Welcome from './components/Welcome'
 import Practice1 from './components/Practice1'
+import TxtInput from './components/TxtInput'
+import CustomComponent from './components/CustomComponent'
 
 const App = () => {
   return (
     <View>
       {/* <Information/> */}
       {/* <Welcome></Welcome> */}
-      <Practice1></Practice1>
+      {/* <Practice1></Practice1> */}
+      {/* <TxtInput></TxtInput> */}
+      <CustomComponent/>
     </View>
   )
 }
